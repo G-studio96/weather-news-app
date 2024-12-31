a1470406cf1b4dd205a7a1ee47b243295a2f3419
gtom <114338392+G-studio96@users.noreply.github.com>
2024-12-31 03:31:01 +0000

    Add weather API service to fetch location-based weather data

--- src/app/services/weatherapi.service.ts
+++ src/app/services/weatherapi.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherapiService {
 
-  constructor() { }
+  private apiUrl = "https://api.openweathermap.org/data/3.0/onecall"; 
+
+  constructor(private http: HttpClient) { }
+
+  getlocationWeather(lat: number, lon: number, part: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}?lat=${lat}&lon=${lon}&exclude=${part}&appid=${environment.weatherApiKey}`)
 }
