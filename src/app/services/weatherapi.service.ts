939add53db7db2d74c8d5840f4a519097e075727
gtom <114338392+G-studio96@users.noreply.github.com>
2025-01-04 14:00:26 +0000

    api request fixed. I was using 3.0 restapi docs. had modify the string to specific of v2.5

--- src/app/services/weatherapi.service.ts
+++ src/app/services/weatherapi.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { UnitSystem } from './setting.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherapiService {
 
-  private apiUrl = "https://api.openweathermap.org/data/3.0/onecall";
+  private apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
   constructor(private http: HttpClient) { }
 
   getlocationWeather(lat: number, lon: number, part: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?lat=${lat}&lon=${lon}&exclude=${part}&appid=${environment.weatherApiKey}`)
+    return this.http.get<any[]>(`${this.apiUrl}?lat=${lat}&lon=${lon}&unit=${UnitSystem}}&appid=${environment.weatherApiKey}`)
   }
 
 }
